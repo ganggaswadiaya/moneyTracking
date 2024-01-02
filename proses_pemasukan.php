0ff104c2b6a3fd5d2ab6cb84aeddb84de3e0e24e
kadekdarmayasa <darmayasadiputra@gmail.com>
2024-01-02 12:17:24 +0800

    menambahkan fungsionalitas dari pemasukkan dan pengeluaran

--- proses_pemasukan.php
+++ proses_pemasukan.php
@@ -7,13 +7,20 @@ $id_dana = $_POST['id_dana'];
 $penghsln = $_POST['penghasilan'];
 $tgl = $_POST['tanggal'];
 
-$query = "INSERT INTO tb_pemasukan values('','$sumber','$id_dana', '$penghsln', '$tgl')";
+$query = "INSERT INTO tb_pemasukan values(null, '$sumber','$id_dana', '$penghsln', '$tgl')";
 
 $hasil = mysqli_query($conn, $query);
 
 if (!$hasil) {
     die("Gagal menambahkan pemasukan" . mysqli_error($conn));
 } else {
+    $query_saldo_dana = "SELECT saldo FROM tb_dana WHERE id_dana=$id_dana";
+    $saldo_dana = (int) mysqli_fetch_assoc(mysqli_query($conn, $query_saldo_dana))['saldo'];
+
+    $saldo_dana = $saldo_dana + $penghsln;
+    $query_update_dana = "UPDATE tb_dana SET saldo=$saldo_dana WHERE id_dana=$id_dana";
+    $update_dana = mysqli_query($conn, $query_update_dana);
+
     echo "<script>alert('Berhasil menambahkan pemasukan');
         location.href='riwayat_pemasukan.php';</script>";
 }
--- proses_pengeluaran.php
+++ proses_pengeluaran.php
@@ -9,12 +9,18 @@ $jmlh = $_POST['jumlah'];
 $total = $jmlh * $hrg;
 $tgl = $_POST['tanggal'];
 
-$query = "INSERT INTO tb_pengeluaran values('','$id_dana','$untk', '$hrg', '$jmlh', '$total', '$tgl')";
+$query = "INSERT INTO tb_pengeluaran values(null,'$id_dana','$untk', '$hrg', '$jmlh', '$total', '$tgl')";
 $hasil = mysqli_query($conn, $query);
 
 if (!$hasil) {
     die("Gagal menambahkan pemasukan" . mysqli_error($conn));
 } else {
+    $query_saldo_dana = "SELECT saldo FROM tb_dana WHERE id_dana=$id_dana";
+    $saldo_dana = (int) mysqli_fetch_assoc(mysqli_query($conn, $query_saldo_dana))['saldo'];
+
+    $saldo_dana = $saldo_dana - $total;
+    $query_update_dana = "UPDATE tb_dana SET saldo=$saldo_dana WHERE id_dana=$id_dana";
+    $update_dana = mysqli_query($conn, $query_update_dana);
     echo "<script>alert('Berhasil menambahkan pengeluaran');
         location.href='riwayat_pengeluaran.php';</script>";
 }
